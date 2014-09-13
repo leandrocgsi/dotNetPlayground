d4a632a096cabd7aad9d488912e1c229264ba0ad
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2014-09-12 22:31:47 -0300

    Initial checkin

---
+++ MyFirstApp/MyFirstApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello C# - We are here!", "My Message Box");
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are hovering your mouse over the button", "My Message Box");
+        }
+    }
+}
