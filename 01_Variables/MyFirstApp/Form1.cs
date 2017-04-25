e857328c7350adb5922ecf36981698dbd50d5335
leandrocgsi <leandrocgsi@gmail.com>
2017-04-25 15:39:01 -0300

    Working with strings

---
+++ 01_Variables/MyFirstApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = "Leandro da Costa";
+            MessageBox.Show("Hello C# - We are here! " + name, "My Message Box");
+        }
+
+    }
+}
