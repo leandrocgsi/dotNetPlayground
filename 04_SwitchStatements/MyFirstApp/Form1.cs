09cfe798e6c1ea01f667438e3898ccc62af96d07
leandrocgsi <leandrocgsi@gmail.com>
2017-05-27 16:45:41 -0300

    Working with Switch statements

---
+++ 04_SwitchStatements/MyFirstApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Equals("Leandro"))
+            {
+                MessageBox.Show("Hello");
+            } else if (!textBox1.Text.Equals("Flávio"))
+            {
+                MessageBox.Show("Hello Flávio");
+            } else 
+            {
+                MessageBox.Show("Hello Leandro");
+            }
+        }
+    }
+}
