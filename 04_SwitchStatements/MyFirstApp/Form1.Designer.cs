3e5dfc0e2f1dd6151f614e77c1727ae877c5bcce
leandrocgsi <leandrocgsi@gmail.com>
2017-05-28 16:49:28 -0300

    Working with Switch statements

--- 04_SwitchStatements/MyFirstApp/Form1.Designer.cs
+++ 04_SwitchStatements/MyFirstApp/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,19 @@
             this.textBox1.Size = new System.Drawing.Size(355, 22);
             this.textBox1.TabIndex = 1;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(354, 22);
+            this.textBox2.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 322);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,6 +76,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
