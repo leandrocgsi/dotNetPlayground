c20f146e86ea29550b6f871b838b5bd75238a2da
leandrocgsi <leandrocgsi@gmail.com>
2017-06-10 17:06:44 -0300

    Working with matemathical operators

--- 05_MathematicalOperators/MyFirstApp/Form1.cs
+++ 05_MathematicalOperators/MyFirstApp/Form1.cs
@@ -26,10 +26,14 @@ namespace MyFirstApp
             //int a = 5;
             //int b = 3;
             //MessageBox.Show((a - b).ToString());
-            
+
+            //int a = 5;
+            //int b = 3;
+            //MessageBox.Show((a * b).ToString());
+
             int a = 5;
             int b = 3;
-            MessageBox.Show((a * b).ToString());
+            MessageBox.Show((a / b).ToString());
         }
     }
 }
