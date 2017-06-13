b9d6ff7494be829aa62b55a1b7a34554f79aa041
leandrocgsi <leandrocgsi@gmail.com>
2017-06-13 17:15:22 -0300

    Working with arrays

---
+++ 06_Arrays/MyFirstApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int a = 5;
+            //int b = 3;
+            //MessageBox.Show((a + b).ToString());
+
+            //int a = 5;
+            //int b = 3;
+            //MessageBox.Show((a - b).ToString());
+
+            //int a = 5;
+            //int b = 3;
+            //MessageBox.Show((a * b).ToString());
+
+            //int a = 5;
+            //int b = 3;
+            //MessageBox.Show((a / b).ToString());
+
+            int a = 5;
+            
+            //add
+            //a = a + 4;
+            a += 4;
+            MessageBox.Show(a.ToString());
+        }
+    }
+}
