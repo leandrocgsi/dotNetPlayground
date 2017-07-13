727d26ca9a1178c993a4cb8f38ecf28c28d59ff0
leandrocgsi <leandrocgsi@gmail.com>
2017-07-13 17:38:27 -0300

    Working with do and do while

---
+++ 09_DoAndDoWhile/MyFirstApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show("Hello " + i.ToString());
+            }
+             */
+            //string[] names = { "Leandro", "Flávio", "Ana" };
+            List<string> names = new List<string>();
+            names.add("Leandro");
+            names.add("Flávio");
+            names.add("Ana");
+
+            foreach (string s in names)
+            {
+                MessageBox.Show("Hello " + s);
+            }
+        }
+    }
+}
