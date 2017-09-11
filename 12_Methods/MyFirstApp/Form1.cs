aa9c076c2d81ca4e8ce3aa24c8168a30958d7fb9
leandrocgsi <leandrocgsi@gmail.com>
2017-09-11 20:49:37 -0300

    Working with Methods

--- 12_Methods/MyFirstApp/Form1.cs
+++ 12_Methods/MyFirstApp/Form1.cs
@@ -19,28 +19,14 @@ namespace MyFirstApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //message();
-            //message("Hello Leandro");
             message("Hello Leandro", "Hello");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //message();
-            //message("Hello Flávio");
             message("Hello Flávio", "Hello");
         }
-        /*
-        void message()
-        {
-            MessageBox.Show("Hello");
-        }
-        */
-        /*void message(string message)
-        {
-            MessageBox.Show(message);
-        }
-        */
+
         void message(string message, string title)
         {
             MessageBox.Show(message, title);
