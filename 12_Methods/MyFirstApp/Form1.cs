be892bf1149d645b0d1e505b2e3bda45e37934b1
leandrocgsi <leandrocgsi@gmail.com>
2017-09-24 21:01:47 -0300

    Working with Methods

--- 12_Methods/MyFirstApp/Form1.cs
+++ 12_Methods/MyFirstApp/Form1.cs
@@ -12,12 +12,24 @@ namespace MyFirstApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(myMethod("Leandro"));
+            //MessageBox.Show(myMethod("Leandro"));
+            //MessageBox.Show(myMethod(1).ToString());
+            MessageBox.Show(myMethod(true).ToString());
         }
 
         string myMethod(string name)
         {
-            return name;
+            return "Hello " + name;
+        }
+
+        int myMethod(int number)
+        {
+            return number;
+        }
+
+        bool myMethod(bool value)
+        {
+            return value;
         }
     }
 }
